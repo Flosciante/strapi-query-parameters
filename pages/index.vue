8ee07ddaf39899422154a6b796c3daf99696025f
Florent <flo@nuxtjs.com>
2020-08-06 11:04:09 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <button v-on:click="getShowcases">Showcases</button>
+  <pre>{{ showcases }}</pre>
+  </main>
+</template>
+
+<script>
+  export default {
+    methods: {
+      //first method
+       async getShowcases() {
+        this.showcases = await this.$strapi.$showcases.find([['plugins.slug', 'vue-routeur'], ['plugins.slug', 'vue-meta']])
+      },
+      //second method
+      /* async getShowcases() {
+        this.showcases = await this.$strapi.$showcases.find({ 'plugins.slug': ['vue-routeur', 'vue-meta'] })
+      },*/
+    },
+    data() {
+      return {
+        showcases: []
+      }
+    }
+  }
+</script>
